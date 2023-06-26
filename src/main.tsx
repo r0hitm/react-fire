25774a8063ea5bac8620dc70c04d85106a12e4b7
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-06-26 15:16:01 +0530

    Added basic routing code

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Routes:
+import App from "./routes/App.tsx";
+import Login from "./routes/Login.tsx";
+import Register from "./routes/Register.tsx";
+import ResetPassword from "./routes/ResetPassword.tsx";
+import ErrorPage from "./routes/ErrorPage.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/register",
+        element: <Register />,
+    },
+    {
+        path: "/reset-password",
+        element: <ResetPassword />,
+    },
+    {
+        // Pathless route is visited when no other route matches:
+        element: <ErrorPage />,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
