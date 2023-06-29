2d66926f787d2afaf313c65c47718737151a4ff6
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-06-30 00:37:02 +0530

    updated basename for deploying on GH pages

--- src/main.tsx
+++ src/main.tsx
@@ -15,46 +15,49 @@ import { ProtectedRoute, UnauthenticatedRoute } from "./RouteProtection.tsx";
 // actions for the routes:
 import { registerAction, loginAction } from "./actions.ts";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: (
-            <ProtectedRoute>
-                <App />
-            </ProtectedRoute>
-        ),
-    },
-    {
-        path: "/login",
-        element: (
-            <UnauthenticatedRoute>
-                <Login />
-            </UnauthenticatedRoute>
-        ),
-        action: loginAction,
-    },
-    {
-        path: "/register",
-        element: (
-            <UnauthenticatedRoute>
-                <Register />
-            </UnauthenticatedRoute>
-        ),
-        action: registerAction,
-    },
-    {
-        path: "/reset-password",
-        element: (
-            <UnauthenticatedRoute>
-                <ResetPassword />
-            </UnauthenticatedRoute>
-        ),
-    },
-    {
-        // Pathless route is visited when no other route matches:
-        element: <ErrorPage />,
-    },
-]);
+const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: (
+                <ProtectedRoute>
+                    <App />
+                </ProtectedRoute>
+            ),
+        },
+        {
+            path: "/login",
+            element: (
+                <UnauthenticatedRoute>
+                    <Login />
+                </UnauthenticatedRoute>
+            ),
+            action: loginAction,
+        },
+        {
+            path: "/register",
+            element: (
+                <UnauthenticatedRoute>
+                    <Register />
+                </UnauthenticatedRoute>
+            ),
+            action: registerAction,
+        },
+        {
+            path: "/reset-password",
+            element: (
+                <UnauthenticatedRoute>
+                    <ResetPassword />
+                </UnauthenticatedRoute>
+            ),
+        },
+        {
+            // Pathless route is visited when no other route matches:
+            element: <ErrorPage />,
+        },
+    ],
+    { basename: "/react-fire" }
+);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
