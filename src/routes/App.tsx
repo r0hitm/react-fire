c68437b445ba367ef71444759dc24fb8f8a6c400
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-06-30 00:00:51 +0530

    Implemented App content and logout button

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,3 +1,20 @@
+import { useContext } from "react";
+import AuthContext from "../AuthContext";
+import { logout } from "../firebase";
+
 export default function App() {
-    return <>App</>;
+    const user = useContext(AuthContext);
+
+    return (
+        <div className="app">
+            <h1>App</h1>
+            <p>Welcome, {user?.email}!</p>
+            <p>
+                This is a protected route. If you are not logged in, you will be
+                redirected to the login page. If you are logged in, you will see
+                this page.
+            </p>
+            <button onClick={logout}>Log Out</button>
+        </div>
+    );
 }
